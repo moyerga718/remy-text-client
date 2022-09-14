b8bf8c9bac5092d9e1bf0d8fa3f93aadf1870388
Griffin Moyer <moyerga@outlook.com>
2022-09-14 08:56:44 -0500

    changed some wording in about

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -7,16 +7,18 @@ export const About = () => {
                 <p className="about-text">The skin on your ankle breaks when you go to scratch this time. Three months of summer went by and you still can't resist the urge.</p>
                 <p className="about-text">You look up and see Remy a little ways ahead. He's gotten so thin you wonder if the kite he's flying could carry him out of this field. Hopefully not. It'd be a shame if his visit was cut short.</p>
                 <p className="about-text">Then you remember his visit is ending anyway. Tomorrow is August 24th—Remy's last day here and your first day of second grade.</p>
-                <p className="about-text">Something changes. Your vision fixes on the brown spot in the grass by your shoe. The back of your throat tastes like copper. The air gets heavy and hard to inhale. It feels like people are watching from the treeline.</p>
+                <p className="about-text">Something changes. Your vision fixes on the brown spot in the grass by your shoe. The back of your throat tastes like copper. The air gets heavy and hard to inhale. Eyes in the treeline.</p>
                 <p className="about-text">You manage to snap out of it, but you're shook. You run up to Remy and try to keep your voice from shaking.</p>
                 <p className="about-text">"Do I really have to go tomorrow? Do YOU really have to go tomorrow?"</p>
                 <p className="about-text">He looks down and gives you that patient, knowing smile that always comforts you. He reels in the kite and kneels down next to you with some difficulty. After a short sigh, he says</p>
                 <p className="about-text">"Don't tell your dad I said this but... no. We don't have to go if we don't want to. We should, we've got obligations to fulfill, but you never HAVE to do anything that you don't want to do. That's an important distinction to make."</p>
-                <p className="about-text">... </p>
-                <p className="about-text">"Think of Dot. Dot is a great pup, real loyal and all, but we seem to forget that she's got teeth that could tear you to shreds. She could do some serious damage if she wanted to.
+                <p className="about-text">...</p>
+                <p className="about-text">"Think about Dot. Dot's a great pup, real loyal and all, but we seem to forget that she's got teeth that could tear you to shreds. She could do some serious damage if she wanted to.
                     And you know what else? We seem to forget that WE have teeth too."</p>
                 <p className="about-text">He notices that his speech hasn't eased the worried look in your eye and softens his tone.</p>
-                <p className="about-text">"Look I'm not trying to make you scared of Dot and I'm not saying you should skip school tomorrow. I'm just saying, you can do whatever the hell you want. You get one body, you get one life. They're both yours and yours alone. Make your own time."</p>
+                <p className="about-text">"Look I'm not trying to make you scared of Dot and I'm not saying you should skip school tomorrow. I'm just saying, you can do whatever the hell you want. You get one body, you get one life. They're both yours and yours alone."</p>
+                <p className="about-text">...</p>
+                <p className="about-text">"Make time. There's just not enough of it."</p>
             </div>
             <div className="about-picture-div">
                 
